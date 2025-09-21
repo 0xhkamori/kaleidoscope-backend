5a6d49a5b33ad7bdab68ca9469e5bda8d5dff3f6
0xhkamori <hk1337@proton.me>
2025-09-21 15:33:13 +0200

    f

--- app/core/auth.py
+++ app/core/auth.py
@@ -1,4 +1,5 @@
 from jwt import JWT
+from jwcrypto import JWK
 import uuid
 from datetime import datetime, timedelta
 from passlib.context import CryptContext
@@ -7,6 +8,7 @@ from .config import JWT_SECRET_KEY, ACCESS_TOKEN_EXPIRE_MINUTES, REFRESH_TOKEN_E
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 jwt = JWT()
+key = JWK(kty='oct', k=JWT_SECRET_KEY)
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
@@ -21,19 +23,19 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     else:
         expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire, "type": "access"})
-    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY)
+    encoded_jwt = jwt.encode(to_encode, key)
     return encoded_jwt
 
 def create_refresh_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
     to_encode.update({"exp": expire, "type": "refresh"})
-    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY)
+    encoded_jwt = jwt.encode(to_encode, key)
     return encoded_jwt
 
 def decode_token(token: str):
     try:
-        payload = jwt.decode(token, JWT_SECRET_KEY)
+        payload = jwt.decode(token, key)
         return payload
     except Exception:
         return None
